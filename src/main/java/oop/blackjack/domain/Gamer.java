d796a6c82e905aa15e7a50174291343db5064a21
soochan <tncksdl05@gmail.com>
2022-11-27 09:09:33 +0900

    Gamer의 역할 1. 추가로 카드를 받는다. 2. 봅은 카드를 소유한다. 3. 카드를 오픈한다.
    협력 관계에서 메시지를 먼저 생각하라, 그러고 나서 메시지를 수신할 객체를 선택하라

--- src/main/java/oop/blackjack/domain/Gamer.java
+++ src/main/java/oop/blackjack/domain/Gamer.java
@@ -1,25 +1,56 @@
 package oop.blackjack.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * 게이머
+ * Gamer의 역할
+ * 1. 추가로 카드를 받는다.
+ * 2. 봅은 카드를 소유한다.
+ * 3. 카드를 오픈한다.
  */
 public class Gamer {
 
     private List<Card> cards;
 
     /**
+     * 생성자를 통해서 cards를 담을 구현체를 추가
+     */
+    public Gamer() {
+        cards = new ArrayList<>();
+    }
+
+    /**
     * 카드를 뽑는다. (받는다)
+     * 카드를 받을떄마다, 소유한 카드들의 숫자를 확인해야 하니, 카드를 받을떄마다 확인하는 책임을 가지는 메서드를 추가
     */
-    private void receiveCard(Card card) {
+    public void receiveCard(Card card) {
+        this.cards.add(card);
+        this.showCards();
     }
 
     /**
     * 카드 모두를 오픈한다.
+     * 현재 갖고 있는 모든 카드들을 전달하는 역할
     */
     private List<Card> openAllCards() {
-        return null;
+        return this.cards;
+    }
+
+    /**
+     * Gamer의 경우, 사용자가 현재 카드들 총 Point를 보며 카드를 더 뽑을지 말지 결정하게 된다.
+     * 이를 위해서는 Gamer는 현재 카드들의 숫자의 합을 확인할 수 있어야 한다.
+     */
+    private void showCards() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("현재 보유 카드 목록 \n");
+
+        for (Card card : cards) {
+            sb.append(card.toString());
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
     }
 
 
--- src/main/java/oop/blackjack/domain/RealGame.java
+++ src/main/java/oop/blackjack/domain/RealGame.java
@@ -1,5 +1,7 @@
 package oop.blackjack.domain;
 
+import java.util.Scanner;
+
 /**
  * blackjack 게임을 실행시킬 객체
  */
@@ -10,6 +12,7 @@ public class RealGame {
     */
     public void play(){
         System.out.println("=====BlackJack Game Start=====");
+        Scanner sc = new Scanner(System.in);
 
         // 딜러 생성
         Dealer dealer = new Dealer();
@@ -23,10 +26,40 @@ public class RealGame {
         // 규칙 생성
         Rule rule = new Rule();
 
-        // 첫번째 카드를 뽑기 위해
-        //Card card = cardDeck.getCard();
+        playingPhase(sc, cardDeck, gamer);
 
-        System.out.println(cardDeck.toString());
 
+    }
+
+    /**
+     * 카드 뽑는 단계를 분리하였다.
+     * - CardDeck을 통해 카드를 뽑고,
+     * - Gamer가 그 카드를 받고,
+     * - Gamer의 현재 카드를 확인
+     *
+     * 여기서 중요한 점은 Gamer는 CardDeck이 어떤 과정을 거쳐서 카드를 뽑아주는지 모른다는 것이다.
+     * CardDeck 내부에서 (1) 남아 있는 카드 중 하나를 랜덤으로 뽑고, (2) 뽑은 카드는 목록에서 제거 라는 과정을
+     * Gamer가 알 필요는 없다는 것이다.
+     * Gamer는 단지 CardDeck에게 카드 하나를 뽑아 달라는 요청만 하면 되는 것이다.
+     *
+     * 객체는 다른 객체에게 요청을 할때, 이렇게 한뒤에 저렇게 하고 마지막으로 어떻게 해달라 라는 식으로 세세하게 요청해서는 안된다.
+     * 객체는 본인의 역할에 충실하면 된다.
+     * CardDeck은 카드를 뽑아 주는 것에, Gamer는 CardDeck에게 카드를 받는 것에 충실해야 한다.
+     * 만일 각 객체의 책임이 모호하게 구현이 되어 있다면, 차후 변경이 있을 경우 어디까지 수정을 해야하는지 알 수 없는 상황이
+     * 올 수도 있다. 그러므로 다른 객체에게 요청을 하는 일은 최대한 해당 객체를 믿고 맡기는 것이 좋다.
+     */
+    private void playingPhase(Scanner sc, CardDeck cardDeck, Gamer gamer) {
+        String gamerInput;
+        while(true) {
+            System.out.println("카드를 뽑겠습니까?? 종료를 원하시면 0을 입력하세요");
+            gamerInput = sc.nextLine();
+
+            if(gamerInput.equals("0")) {
+                break;
+            }
+
+            Card card = cardDeck.draw();
+            gamer.receiveCard(card);
+        }
     }
 }

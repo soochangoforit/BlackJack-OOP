83dd35ceb3643fb2cca08b7daa6656c6311d77c7
soochan <tncksdl05@gmail.com>
2022-12-01 21:20:30 +0900

    [Feat] 딜러와 게이머 모두 카드 1장을 갖는 책임 추가 - 딜러와 게이머는 CardDeck으로 뽑는 책임을 위임하고, 뽑은 카드들을 자신이 관리 하는 책임을 갖는다.

--- src/main/java/oop/blackjackv2/domain/Dealer.java
+++ src/main/java/oop/blackjackv2/domain/Dealer.java
@@ -1,4 +1,26 @@
 package oop.blackjackv2.domain;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Dealer {
+
+    private CardDeck cardDeck;
+    private List<Card> myCards = new LinkedList<>();
+
+    public Dealer(CardDeck cardDeck){
+        this.cardDeck = cardDeck;
+    }
+
+    public void initDraw() {
+        addToMine(this.cardDeck.draw());
+    }
+
+    private void addToMine(Card card) {
+        this.myCards.add(card);
+    }
+
+    public List<Card> getMyCards() {
+        return myCards;
+    }
 }

ef8dae16c07806b49d45efaf5862f735b9eb7bc5
soochan <tncksdl05@gmail.com>
2022-12-02 21:10:35 +0900

    [Refactor] 게이머가 21 점수를 초과할 경우에는 더 이상 뽑지 못하도록 변경

--- src/main/java/oop/blackjackv2/domain/Gamer.java
+++ src/main/java/oop/blackjackv2/domain/Gamer.java
@@ -20,7 +20,21 @@ public class Gamer implements Person {
 
     @Override
     public void draw() {
-        addToMine(this.cardDeck.draw());
+        if (isBust()) {
+            System.out.println("버스트!! 점수가 21점 초과이기 때문에, 더 이상 카드를 더 뽑을 수 없습니다.");
+        } else {
+            addToMine(this.cardDeck.draw());
+        }
+    }
+
+    private boolean isBust() {
+        return getPoints() > 21;
+    }
+
+    private int getPoints() {
+        return myCards.stream()
+                .mapToInt(Card::getPoint)
+                .sum();
     }
 
     private void addToMine(Card card) {

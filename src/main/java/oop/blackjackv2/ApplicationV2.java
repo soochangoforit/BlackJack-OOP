8b2ae10e4a1fcb91e45722cf153022f6d701a168
soochan <tncksdl05@gmail.com>
2022-12-03 13:21:26 +0900

    [Feat] 딜러와 게이머가 게임을 진행하면서 승자를 가리는 전체적인 책임 추가
    - 하나의 메서드는 하나의 책임만 갖도록 노력하였다.
    - indent(인덴트, 들여쓰기) depth를 2가 넘지 않도록 구현한다. 가능한 1까지만 허용한다.

--- src/main/java/oop/blackjackv2/ApplicationV2.java
+++ src/main/java/oop/blackjackv2/ApplicationV2.java
@@ -1,4 +1,11 @@
 package oop.blackjackv2;
 
+import oop.blackjackv2.domain.RealGame;
+
 public class ApplicationV2 {
+
+    public static void main(String[] args) {
+        RealGame realGame = new RealGame();
+        realGame.play();
+    }
 }
--- src/main/java/oop/blackjackv2/domain/RealGame.java
+++ src/main/java/oop/blackjackv2/domain/RealGame.java
@@ -1,4 +1,84 @@
 package oop.blackjackv2.domain;
 
 public class RealGame {
+
+    public void play(){
+        Card card = new Card();
+        CardDeck cardDeck = new CardDeck(card);
+        cardDeck.createCards();
+
+        Gamer gamer = new Gamer(cardDeck,"게이머1");
+        Dealer dealer = new Dealer(cardDeck);
+
+        Rule rule = new Rule(gamer, dealer);
+        rule.initialDraw();
+
+        View view = new View(gamer, dealer);
+        view.showInitialDrawInfo();
+
+        // 입력값에 따라 더 뽑을지 말지 결정
+        boolean isGamerTurn = true;
+        boolean isDealerTurn = true;
+
+        while (isGamerTurn || isDealerTurn) {
+            isGamerTurn = GamerTurn(gamer, rule, view);
+            view.showInitialDrawInfo();
+            
+            isDealerTurn = DealerTurn(dealer, rule, view);
+            view.showInitialDrawInfo();
+        }
+
+        Person winner = rule.findWinner();
+        view.showWinner(winner);
+        
+    }
+
+    private boolean DealerTurn(Dealer dealer, Rule rule, View view) {
+        if (rule.isContinue(view.getInputYesOrNo(dealer))){
+            return draw(dealer, view);
+        }
+        return !checkCanStop(dealer, view);
+    }
+
+    private boolean checkCanStop(Dealer dealer, View view) {
+        if (isUnderMinLimit(dealer, view)) {
+            dealer.draw();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean draw(Dealer dealer, View view) {
+        if (isOverMaxLimit(dealer, view)) {
+            return false;
+        }
+        dealer.draw();
+        return true;
+    }
+
+    private boolean isUnderMinLimit(Dealer dealer, View view) {
+        if (dealer.isUnderLimit()){
+            view.show("딜러의 점수가 16점 이하이기 때문에, 카드를 한 장 더 뽑습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isOverMaxLimit(Dealer dealer,View view) {
+        if (dealer.isMaxLimit()) {
+            view.show("딜러는 17점 이상이기 때문에, 더 이상 카드를 뽑을 수 없습니다.");
+            return true;
+        }
+        return false;
+    }
+
+    private boolean GamerTurn(Gamer gamer, Rule rule, View view) {
+        if (rule.isContinue(view.getInputYesOrNo(gamer))){
+            gamer.draw();
+            return true;
+        }
+        return false;
+    }
+
+
 }
